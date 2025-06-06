08a56e2dae43915c08a25feab21ff2ab47af9945
Estella1 <estella.santos.moreira@gmail.com>
2025-06-06 14:34:00 -0300

    Estrutura do sal e pimenta

---
+++ src/main/java/FilterSaltPepper.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class FilterSaltPepper {
+    //Filtro que remove os ruídos da imagem(pontos brancos e pretos aleatórios que aparecem na imagem)
+    public static byte[][][] removerSalPimenta(byte[][][] image) {
+        return null;
+    }
+    //Filtro que melhora as partes que estão embaçadas ou arrastadas
+    public static byte[][][] removerBorroestempo(byte[][][] image) {
+        return null;
+    }
+    private static int converteByteInteiro(byte b) {
+        return 0;
+    };
+    private static byte converteIntByte(byte b) {
+        return 0;
+    }
+    private static int media(ArrayList<Integer> array) {//nao tenho certeza se precisa ser um array de entrada
+
+        return 0;
+    }
+    private static byte [][] preencheFrameZeros(byte [][][] image, int frame) {
+        return null;
+    }
+}
